5620599a7f790cca6fafca70f3781a293391e30d
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-01-08 20:18:37 -0700

    favorite_groomer added to migrations and seed

--- data/migrations/20201013074025_customers.js
+++ data/migrations/20201013074025_customers.js
@@ -14,7 +14,7 @@ exports.up = (knex) => {
     table.string('country', 128);
     table.string('photo_url');
     table.timestamps(true, true);
-    table.specificType('stringarray', 'favorite_groomers ARRAY');
+    table.specificType('favorite_groomers', 'text ARRAY');
   });
 };
 
