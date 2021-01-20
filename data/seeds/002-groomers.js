583b7377f9a4a6f3c0cdec62064177903b1fb352
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2021-01-19 19:35:02 -0500

    Commented lines 20 through 41 of groomers seed file

--- data/seeds/002-groomers.js
+++ data/seeds/002-groomers.js
@@ -17,28 +17,28 @@ const faker = require('faker');
 //   vet_visit_rate: faker.random.number(30000),
 // }));
 
-const groomers = [
-  {
-    photo_url: faker.image.avatar(),
-    name: faker.name.firstName(),
-    lastname: faker.name.lastName(),
-    description: faker.lorem.paragraph(1),
-    address: '499-401 Beech Ave',
-    latitude: '37.619167',
-    longitude: '-122.417930',
-    zip: '94066',
-    phone: faker.phone.phoneNumber(),
-    email: faker.internet.email(),
-    city: 'San Bruno',
-    state: 'California',
-    country: 'USA',
-    walk_rate: faker.random.number(30000),
-    day_care_rate: faker.random.number(30000),
-    vet_visit_rate: faker.random.number(30000),
-    dogs: true,
-    cats: false,
-  },
-];
+// const groomers = [
+//   {
+//     photo_url: faker.image.avatar(),
+//     name: faker.name.firstName(),
+//     lastname: faker.name.lastName(),
+//     description: faker.lorem.paragraph(1),
+//     address: '499-401 Beech Ave',
+//     latitude: '37.619167',
+//     longitude: '-122.417930',
+//     zip: '94066',
+//     phone: faker.phone.phoneNumber(),
+//     email: faker.internet.email(),
+//     city: 'San Bruno',
+//     state: 'California',
+//     country: 'USA',
+//     walk_rate: faker.random.number(30000),
+//     day_care_rate: faker.random.number(30000),
+//     vet_visit_rate: faker.random.number(30000),
+//     dogs: true,
+//     cats: false,
+//   },
+// ];
 const groomers = [...new Array(100)].map(() => ({
   photo_url: faker.image.avatar(),
   name: faker.name.firstName(),
